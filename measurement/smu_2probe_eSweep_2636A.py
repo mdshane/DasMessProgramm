32dd11b696b5d3aff930166bac2f91d2322537a7
mdshane <hanefeld@uni-frankfurt.de>
2019-02-11 14:29:13 +0100

    Exported gpib-Instrument class to own file.

--- measurement/smu_2probe_eSweep_2636A.py
+++ measurement/smu_2probe_eSweep_2636A.py
@@ -1,5 +1,7 @@
 from .measurement import register, AbstractMeasurement, Contacts, PlotRecommendation
 from .measurement import StringValue, FloatValue, IntegerValue, DatetimeValue, AbstractValue, SignalInterface, GPIBPathValue
+from .measurement.gpib_Instrument import *
+
 
 import numpy as np
 from datetime import datetime
@@ -12,116 +14,6 @@ from .smu_2probe_2636A import SMU2Probe2636A
 from scientificdevices.oxford.itc503 import ITC
 import gpib
 
-
-class GenericInstrument(object):
-    """ This is an abstract class for a generic instrument """
-    def __init__(self):
-        """ Initialises the generic instrument """
-        self.term_chars = '\n'
-
-    def ask(self, query):
-        """ ask will write a request and waits for an answer
-
-            Arguments:
-            query -- (string) the query which shall be sent
-
-            Result:
-            (string) -- answer from device
-        """
-        self.write(query)
-        return self.read()
-
-    def write(self, query):
-        """ writes a query to remote device
-
-            Arguments:
-            query -- (string) the query which shall be sent
-        """
-        pass
-
-    def read(self):
-        """ reads a message from remote device
-
-            Result:
-            (string) -- message from remote device
-        """
-        pass
-
-    def close(self):
-        """ closes connection to remote device """
-        pass
-
-class GpibInstrument(GenericInstrument):
-    """ Implementation of GenericInstrument to communicate with gpib devices """
-    def __init__(self, device):
-        """ initializes connection to gpib device
-
-            Arguments:
-            connection - (gpib.dev) a gpib object to speak to
-        """
-        GenericInstrument.__init__(self)
-        self.device = device
-        self.term_chars = '\n'
-
-    def write(self, query):
-        """ writes a query to remote device
-
-            Arguments:
-            query -- (string) the query which shall be sent
-        """
-        gpib.write(self.device, query + self.term_chars)
-
-    def read(self):
-        """ reads a message from remote device
-
-            Result:
-            (string) -- message from remote device
-        """
-        return gpib.read(self.device, 512).rstrip()
-
-    def close(self):
-        """ closes connection to remote device """
-        gpib.close(self.device)
-
-    def clear(self):
-        """ clears all communication buffers """
-        gpib.clear(self.device)
-
-def get_gpib_timeout(timeout):
-    """ returns the correct timeout object to a certain timeoutvalue
-        it will find the nearest match, e.g., 120us will be 100us
-
-        Arguments:
-        timeout -- (float) number of seconds to wait until timeout
-    """
-    gpib_timeout_list = [(0, gpib.TNONE), \
-                         (10e-6, gpib.T10us), \
-                         (30e-6, gpib.T30us), \
-                         (100e-6, gpib.T100us), \
-                         (300e-6, gpib.T300us), \
-                         (1e-3, gpib.T1ms), \
-                         (3e-3, gpib.T3ms), \
-                         (10e-3, gpib.T10ms), \
-                         (30e-3, gpib.T30ms), \
-                         (100e-3, gpib.T100ms), \
-                         (300e-3, gpib.T300ms), \
-                         (1, gpib.T1s), \
-                         (3, gpib.T3s), \
-                         (10, gpib.T10s), \
-                         (30, gpib.T30s), \
-                         (100, gpib.T100s), \
-                         (300, gpib.T300s), \
-                         (1000, gpib.T1000s)]
-
-    for val, res in gpib_timeout_list:
-        if timeout <= val:
-            return res
-    return gpib.T1000s
-
-def get_gpib_device(port: int, timeout=0.5):
-    device = gpib.dev(0, port)
-    gpib.timeout(device, get_gpib_timeout(timeout))
-    return GpibInstrument(device)
 
 
 
