a9c7dd56247869799c4bdde1bf87c6af6eebee09
mdshane <hanefeld@uni-frankfurt.de>
2019-02-18 17:01:58 +0100

    Added possibility to update the sensitivity automatically.

--- measurement/srs_temp_sweep.py
+++ measurement/srs_temp_sweep.py
@@ -8,6 +8,7 @@ from visa import ResourceManager
 from scientificdevices.stanford_research_systems.sr830m import SR830m
 
 from scientificdevices.oxford.itc503 import ITC
+from .gpib_Instrument import *
 
 from datetime import datetime
 from time import sleep, time
@@ -21,116 +22,6 @@ import gpib
 import traceback
 
 from ast import literal_eval
-
-class GenericInstrument(object):
-    """ This is an abstract class for a generic instrument """
-    def __init__(self):
-        """ Initialises the generic instrument """
-        self.term_chars = '\n'
-
-    def ask(self, query):
-        """ ask will write a request and waits for an answer
-
-            Arguments:
-            query -- (string) the query which shall be sent
-
-            Result:
-            (string) -- answer from device
-        """
-        self.write(query)
-        return self.read()
-
-    def write(self, query):
-        """ writes a query to remote device
-
-            Arguments:
-            query -- (string) the query which shall be sent
-        """
-        pass
-
-    def read(self):
-        """ reads a message from remote device
-
-            Result:
-            (string) -- message from remote device
-        """
-        pass
-
-    def close(self):
-        """ closes connection to remote device """
-        pass
-
-class GpibInstrument(GenericInstrument):
-    """ Implementation of GenericInstrument to communicate with gpib devices """
-    def __init__(self, device):
-        """ initializes connection to gpib device
-
-            Arguments:
-            connection - (gpib.dev) a gpib object to speak to
-        """
-        GenericInstrument.__init__(self)
-        self.device = device
-        self.term_chars = '\n'
-
-    def write(self, query):
-        """ writes a query to remote device
-
-            Arguments:
-            query -- (string) the query which shall be sent
-        """
-        gpib.write(self.device, query + self.term_chars)
-
-    def read(self):
-        """ reads a message from remote device
-
-            Result:
-            (string) -- message from remote device
-        """
-        return gpib.read(self.device, 512).rstrip()
-
-    def close(self):
-        """ closes connection to remote device """
-        gpib.close(self.device)
-
-    def clear(self):
-        """ clears all communication buffers """
-        gpib.clear(self.device)
-
-def get_gpib_timeout(timeout):
-    """ returns the correct timeout object to a certain timeoutvalue
-        it will find the nearest match, e.g., 120us will be 100us
-
-        Arguments:
-        timeout -- (float) number of seconds to wait until timeout
-    """
-    gpib_timeout_list = [(0, gpib.TNONE), \
-                         (10e-6, gpib.T10us), \
-                         (30e-6, gpib.T30us), \
-                         (100e-6, gpib.T100us), \
-                         (300e-6, gpib.T300us), \
-                         (1e-3, gpib.T1ms), \
-                         (3e-3, gpib.T3ms), \
-                         (10e-3, gpib.T10ms), \
-                         (30e-3, gpib.T30ms), \
-                         (100e-3, gpib.T100ms), \
-                         (300e-3, gpib.T300ms), \
-                         (1, gpib.T1s), \
-                         (3, gpib.T3s), \
-                         (10, gpib.T10s), \
-                         (30, gpib.T30s), \
-                         (100, gpib.T100s), \
-                         (300, gpib.T300s), \
-                         (1000, gpib.T1000s)]
-
-    for val, res in gpib_timeout_list:
-        if timeout <= val:
-            return res
-    return gpib.T1000s
-
-def get_gpib_device(port: int, timeout=0.5):
-    device = gpib.dev(0, port)
-    gpib.timeout(device, get_gpib_timeout(timeout))
-    return GpibInstrument(device)
 
 
 @register('SRS830 Resistance vs. Temp. (blue)')
@@ -160,9 +51,12 @@ class SRS830RvTBlue(AbstractMeasurement):
             self.abort()
             return   
             
+        # temperature control related variables
         self._temperature_end = temperature_end
-        
         self._last_toggle = time()
+
+        # Lock-In control related variables
+        self._sens_check_time = time()
         
         sleep(1)
 
@@ -235,6 +129,18 @@ class SRS830RvTBlue(AbstractMeasurement):
         resistance = x / self._device.slvl * self._pre_resistance
         
         self._signal_interface.emit_data({'R': resistance, 'T': T3})
+
+        self._check_sensitivitiy(r, sensitivity)
+
+    def _check_sensitivitiy(self, r, sensitivity):
+        if time() > self._sens_check_time + 10:
+            sensitivity_range_max = sensitivity_list(sensitivity)
+            if r > 0.9 * sensitivity_range_max:
+                self._device.sens = sensitivity - 1
+            elif r < 0.45 * sensitivity_range_max:
+                self._device.sens =  sensitivity + 1
+            self._sens_check_time = time()
+            print('DEBUG: Sensitivity checked.')
         
         
 
